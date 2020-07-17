570ed10efe540558709b5b1c5778d2e09e421a40
abdullah <hbkamk@gmail.com>
2020-07-17 19:45:14 +0600

    Updates

---
+++ precache-manifest.01aacd2070ac50d351d57e14008efbbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aee4d26beb4bc6fe246270c404ec791",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "3458df78e237ba8430ba",
+    "url": "/Tic-Tac-Toe/static/css/main.535aac14.chunk.css"
+  },
+  {
+    "revision": "8a9517ca38a622e484c4",
+    "url": "/Tic-Tac-Toe/static/js/2.05cfafde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.05cfafde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3458df78e237ba8430ba",
+    "url": "/Tic-Tac-Toe/static/js/main.30254971.chunk.js"
+  },
+  {
+    "revision": "e52aaa505ba37bccc14a",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.db1ab52c.js"
+  }
+]);
